7038977c6fd4c3f38fd07eb7c489d50235ad2c25
JoseGCort <josegcort@gmail.com>
2026-05-21 21:01:03 -0500

    Add initial project structure with Spring Boot setup
    (cherry picked from commit ee3833c826feb866efb92db8548d3edd707fd6f5)
    (cherry picked from commit f46a309f5f028bca4bf9ce6b2a18c8450c76dcdd)

---
+++ src/test/java/com/client_microservice/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client_microservice.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
